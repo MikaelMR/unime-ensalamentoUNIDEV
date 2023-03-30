caa9bdff4dcc3fcd0a2968d335d95bd9b1fb3146
natan1563 <natromafe@gmail.com>
2023-03-30 19:18:33 -0300

    init of the vue project

---
+++ src/components/Botao.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="incrementar">
+    Contar {{ contador }}
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "Botao",
+    data() {
+      return {
+        contador: 0
+      }
+    },
+    methods: {
+      incrementar() {
+        this.contador++
+      }
+    }
+  }
+</script>
