e629708115dc361583c9667c8edef73790294835
mikael-version <mikaramos.pro@gmail.com>
2023-04-10 22:42:41 -0300

    Estilização inicial

--- src/components/modules/Buscador.vue
+++ src/components/modules/Buscador.vue
@@ -1,11 +1,16 @@
 <template>
     <section>
-        <div class="main">
-            <div>
+        <div class="buscador-main">
+            <div class="buscador-title">
                 <h1>LOCALIZE A SUA SALA</h1>
                 <p>Informe seu RA, Curso ou Disciplina</p>
             </div>
-            <input type="text" v-model="input" placeholder="Ex.: Medicina Veterinária">
+            <div class="buscador-container">
+                <form action="">
+                    <input type="text" v-model="input" placeholder="Ex.: Medicina Veterinária" name="buscador">
+                    <button type="submit">Buscar sala</button>
+                </form>
+            </div>
         </div>
     </section>
 </template>
@@ -17,5 +22,19 @@
 </script>
 
 <style>
-
+    .buscador-main {
+        background-color: grey;
+        padding: 20px;
+        width: 755px;
+        margin-left: auto;
+        margin-right: auto;
+        border-radius: 5px;
+    }
+    .buscador-title {
+        text-align: left;
+    }
+    input {
+        width: 87%;
+        align-content: left;
+    }
 </style>
